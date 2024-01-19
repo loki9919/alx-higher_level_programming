f825c28ffdf010218b5f1c3f742fb92c0180739b
lokmane rouijel <lokmane9919@gmail.com>
2024-01-18 17:06:23 -0800

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,10 +4,10 @@ if __name__ == "__main__":
 
     ct = len(sys.argv) - 1
     if ct == 0:
-        print("0 argument.")
+        print("0 arguments.")
     elif ct == 1:
         print("1 argument:")
     else:
-        print("{} argument:".format(ct))
+        print("{} arguments:".format(ct))
     for i in range(ct):
         print("{}: {}".format(i + 1, sys.argv[i + 1]))
