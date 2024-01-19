59e0d16382a79595971ad71cd71247eca102dc29
lokmane rouijel <lokmane9919@gmail.com>
2024-01-18 17:30:13 -0800

    hidden discovery

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,6 +2,7 @@
 if __name__ == "__main__":
     import hidden_4
 
-    for i in dir(hidden_4):
-        if i[:2] != "__":
-            print(i)
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
