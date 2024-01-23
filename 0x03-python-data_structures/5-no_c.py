e310125e8234408385552ba1337f79134aa17372
lokmane rouijel <lokmane9919@gmail.com>
2024-01-22 18:05:42 -0800

    no C

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,11 +1,7 @@
 #!/usr/bin/env python3
 def no_c(my_string):
-    new_str = list(my_string)
-    i = 0
-    while i < len(new_str):
-        if new_str[i] == 'c' or new_str[i] == 'C':
-            new_str.pop(i)
-        else:
-            i += 1
-    result_str = ''.join(new_str)
-    return result_str
+    result = ""
+    for i in range(len(my_string)):
+        if (my_string[i] != 'C' and my_string[i] != 'c'):
+            result += my_string[i]
+    return result
