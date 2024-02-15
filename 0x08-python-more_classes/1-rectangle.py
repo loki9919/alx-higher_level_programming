23ff5ee16d211cf03b82299040e4f659e9427d09
lokmane rouijel <lokmane9919@gmail.com>
2024-02-15 00:00:52 +0000

    Real definition of a rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,47 +1,53 @@
 #!/usr/bin/python3
+"""
+1-rectangle.py: has a class Rectangle that defines a rectangle
+"""
 
 
 class Rectangle:
-    """Empty class Rectangle"""
+    """Class Rectangle that defines a Rectangle
+    Attributes:
+        width: width of Rectangule
+        height: height of rectangule
+
+    """
 
     def __init__(self, width=0, height=0):
-        """ intializer
-        Args
-           width
-           height
-        """
-        self.width = width
-        self.height = height
+        """Initializer with default width and height equal 0"""
+        if type(width) is not int:
+            raise TypeError('width must be an integer')
+        if width < 0:
+            raise ValueError('width must be >= 0')
+        if type(height) is not int:
+            raise TypeError('height must be an integer')
+        if height < 0:
+            raise ValueError('height must be >= 0')
+        self.__width = width
+        self.__height = height
 
     @property
     def width(self):
-        """gets width"""
-        return self.__width
-
-    @property
-    def height(self):
-        """gets height"""
-        return self.__height
+        """property to retrieve private instance width"""
+        return (self.__width)
 
     @width.setter
     def width(self, value):
-        """sets width
-        Args:
-        value: width
-        """
-        if not isinstance(value, int):
+        """to set private instance width"""
+        if type(value) is not int:
             raise TypeError('width must be an integer')
         if value < 0:
             raise ValueError('width must be >= 0')
         self.__width = value
 
+    @property
+    def height(self):
+        """property to retrieve private instance height"""
+        return (self.__height)
+
     @height.setter
     def height(self, value):
-        """ sets height
-        Args:
-        value: height
-        """
-        if not isinstance(value, int):
+        """to set private instance height"""
+        if type(value) is not int:
             raise TypeError('height must be an integer')
         if value < 0:
             raise ValueError('height must be >= 0')
