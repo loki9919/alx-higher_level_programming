05e88f73669d09dff2968ccf2ddfea32ee53e827
lokmane rouijel <lokmane9919@gmail.com>
2024-02-19 20:53:08 +0000

    Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""defines a base geometry class BaseGeometry"""
+
+
+class BaseGeometry:
+    """represent base geometry"""
+
+    def area(self):
+        """not implemented"""
+        raise Exception("area() is not implemented")
