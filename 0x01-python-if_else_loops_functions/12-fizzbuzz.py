48ebbaa55c05c7e1749b9a29b6558130935ff58a
lokmane rouijel <lokmane9919@gmail.com>
2024-03-25 20:25:54 -0700

    FizzBuzz

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+def fizzbuzz():
+    for x in range(1, 101):
+        if x % 15 == 0:
+            print("FizzBuzz ", end="")
+        elif x % 5 == 0:
+            print("Buzz ", end="")
+        elif x % 3 == 0:
+            print("Fizz ", end="")
+        else:
+            print("{} ".format(x), end="")
