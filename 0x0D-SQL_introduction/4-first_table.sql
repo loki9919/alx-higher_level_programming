46a79df7d56e1f868470956c3fd3286f4f9d3362
lokmane rouijel <lokmane9919@gmail.com>
2024-03-27 14:40:42 -0700

    first table

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,4 @@
+--  creates a table called first_table in the current database in my MySQL server
+-- id: INT
+-- name VARCHAR(256)
+CREATE TABLE IF NOT EXISTS first_table (`id` INT, `name` VARCHAR(256))
