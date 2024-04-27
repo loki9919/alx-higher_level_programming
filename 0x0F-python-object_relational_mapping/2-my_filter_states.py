8265e6fc7aa9cabdaa16031abcc669d50298d506
lokmane rouijel <lokmane9919@gmail.com>
2024-04-27 23:30:53 +0100

    filter states by user input

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -8,8 +8,8 @@ import MySQLdb
 
 if __name__ == '__main__':
     db = MySQLdb.connect(port=3306, user=argv[1], passwd=argv[2], db=argv[3])
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE '%{0}%'".format(argv[4]))
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE '%{0}%'".format(argv[4]))
     for row in cursor.fetchall():
         print(row)
     cursor.close()
