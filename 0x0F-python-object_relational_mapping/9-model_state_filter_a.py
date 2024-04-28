ecc27312ce8c49eac38ae733ccced8bc98fe3457
lokmane rouijel <lokmane9919@gmail.com>
2024-04-28 03:19:11 +0100

    Contains `a`

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
-    query = session.query(State).filter(State.name.like('%a')).order_by(State.id).all()
+    query = session.query(State).filter(State.name.like('%a%')).all()
 
     for row in query:
         print("{}:{}".format(row.id, row.name))
