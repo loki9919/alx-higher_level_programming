47f8f662f761cfa1ba0a1c4d59b5e8eeeb1a1d44
lokmane rouijel <lokmane9919@gmail.com>
2024-06-05 00:57:54 +0100

    javascript web scraping

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,12 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
-
-request.get(url, (error, body) => {
-  if (error) {
-    console.log(error);
-  } else {
-    const content = JSON.parse(body);
-    console.log(content.title);
-  }
+let url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
+  console.log(error || JSON.parse(body).title);
 });
